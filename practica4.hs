be7ba6ae88365aecea1792297ce73e446d881161
etnicst <94811143+etnicst@users.noreply.github.com>
2024-10-08 15:48:48 -0600

    1 y 2

--- practica4.hs
+++ practica4.hs
@@ -48,10 +48,13 @@ segundoElemento (_, y) = y
 -- 1. Conflicto. Determina si la claúsula vacía, representada como una lista vacia, forma parte del conjunto de cláusulas. Puesto que no existe modelo que satisfaga a la claúsula vacía, la busqueda del modelo falla. 
 
 conflict :: Estado -> Bool
+conflict (_, []) = False
+conflict (_, (x:xs)) = if x == [] then True else conflict ([], xs)
 
 -- 2. Exito. Determina si la búsqueda del modelo ha sido exitosa, esto sucede cuando el conjunto de cláusulas es vacío.
 
 success :: Estado -> Bool
+success (_, clausulas) = if clausulas == [] then True else False
 
 -- 3. Clausula unitaria. Si "l" es una literal que pertenece a una claúsula unitaria, entonces basta con agregar "l" al modelo y seguir con la búsqueda...
 
